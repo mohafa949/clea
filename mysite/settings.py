ba78244cf4b704962e2ff719cf8f32972fb0160c
mohafa949 <142892091+mohafa949@users.noreply.github.com>
2025-09-19 18:36:15 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,5 +144,6 @@ STATICFILES_DIRS = [
 # Media files (uploaded by admin: products, news, partners)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
